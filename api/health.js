f510a249ffd56a13b158a01a653e0e809fa1cd5f
justincichowski <justincichowski@gmail.com>
2025-09-01 21:44:57 -0400

    learning a lot about typecheck -NoEmit =)

--- api/health.js
+++
@@ -1,9 +0,0 @@
-/**
- * A health check endpoint that responds with an "ok" status.
- *
- * @param {VercelRequest} request - The incoming request object.
- * @param {VercelResponse} response - The outgoing response object.
- */
-export default function handler(request, response) {
-    response.status(200).json({ status: 'ok' });
-}
